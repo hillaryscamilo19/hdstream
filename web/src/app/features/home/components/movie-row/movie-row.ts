cbfd4ab416d0f3731d738b4bc5053c6b3b4597e4
hillarys camilo <hcr090120@gmail.com>
2026-05-29 18:02:03 -0400

    cambios

--- web/src/app/features/home/components/movie-row/movie-row.ts
+++ web/src/app/features/home/components/movie-row/movie-row.ts
@@ -1,8 +1,11 @@
 import { Component, input } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CarouselModule } from 'primeng/carousel';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-movie-row',
-  imports: [],
+  imports: [ButtonModule, CarouselModule, TagModule],
   templateUrl: './movie-row.html',
   styleUrl: './movie-row.css',
 })
